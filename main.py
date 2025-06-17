5173458c21ff83009c4850e0f9f819e9307a8ac2
Sangit Gyawali <developersangeet@gmail.com>
2025-06-17 19:27:35 +0545

    c1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("I am learning Python !")
+first_name = "John"
+food = "pizza"
+email = "john@example.com"
+
+age = 30
+print(f"Hello, my name is {first_name}. I am {age} years old and I love {food}. You can contact me at {email}.")
