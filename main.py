8cc53369968b054da6702d2282337236f8545b90
Sangit Gyawali <developersangeet@gmail.com>
2025-06-17 19:32:18 +0545

    Input

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-price = 9.99
-gpa = 3.5
-distance = 100.0
+Price = input("Enter the price: ")
+GPA = input("Enter the GPA: ")
+Distance = input("Enter the distance: ")
 
-is_available = True
-
-print("Price:", price)
-print("GPA:", gpa)
-print("Distance:", distance)
-print("Is Available:", is_available)
-
-if is_available:
-    print("The item is available for purchase.")
-else:
-    print("The item is not available for purchase.")
+print("Price:", Price)
+print("GPA:", GPA)  
+print("Distance:", Distance)
