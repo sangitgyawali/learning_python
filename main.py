cf87dc4561ccdfd0b9b5a81a98f8be4f85baf119
Sangit Gyawali <developersangeet@gmail.com>
2025-06-28 16:31:17 +0545

    Dictionaries in Python

---
+++ main.py
@@ -0,0 +1,13 @@
+# Dictionary in Python
+
+capitals = {
+    "USA": "Washington, D.C.",
+    "France": "Paris",
+    "Japan": "Tokyo",
+    "India": "New Delhi"
+}
+
+print(capitals.get("USA"))
+print(capitals.get("France"))
+print(capitals.get("Japan"))
+print(capitals.get("India")) 
