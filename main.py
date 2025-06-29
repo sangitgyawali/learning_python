571972bf2b462271598621b4eefaf930a992ae77
Sangit Gyawali <developersangeet@gmail.com>
2025-06-29 18:41:37 +0545

    Random Numbers in python

--- main.py
+++ main.py
@@ -1,35 +1,7 @@
-# Concession stand program
+# Random numbers
 
-menu = {
-    "hot_dog": 3.50,
-    "hamburger": 4.00,
-    "fries": 2.50,
-    "drink": 1.50
-}
+import random
 
-cart = []
-total = 0
+number = random.randint(1, 20)
 
-print("------ Menu ------")
-for key, value in menu.items():
-    print(f"{key:10}: ${value:.2f}")
-
-print("-------------------")
-
-while True:
-    food = input("Enter food item to add to cart (or 'done' to finish): ").strip().lower()
-    if food == 'done':
-        break
-    elif food in menu:
-        cart.append(food)
-        total += menu[food]
-        print(f"Added {food} to cart. Current total: ${total:.2f}")
-    else:
-        print("Item not found in menu. Please try again.")
-
-print("\n------ Cart ------")
-for item in cart:
-    print(f"{item:10 }: ${menu[item]:.2f}")
-print("-------------------")
-print(f"Total amount due: ${total:.2f}")    
-
+print(number)
