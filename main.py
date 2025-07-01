f8ba8c61fb8f7a0b8a56dbeca4107ccb6f15a1ab
Sangit Gyawali <developersangeet@gmail.com>
2025-07-01 21:51:30 +0545

    Function operations in Python

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-# Calculator using functions in Python
+# Functions in python
 
-def add(x, y):
-    return x + y
+def create_name(first_name, last_name):
+    first_name = first_name.capitalize()
+    last_name = last_name.capitalize()
+    return f"{first_name} {last_name}"
 
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    if y == 0:
-        return "Error! Division by zero."
-    return x / y
-
-def main():
-    print(add(5, 3))
-    print(subtract(5, 3))
-    print(multiply(5, 3))
-    print(divide(5, 3))
-    print(divide(5, 0))
+full_name = create_name("john", "doe")
+print(full_name)
