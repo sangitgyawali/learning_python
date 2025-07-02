42cece6845283b7b091163d1900221b9bb6ac4cd
Sangit Gyawali <developersangeet@gmail.com>
2025-07-02 19:07:34 +0545

    Iterables in Python

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-# Args and kwargs in Python
-# Kwargs
+# Iterables in Python
 
-def shipping_label(*args , **kwargs):
-    for arg in args:
-        print(arg, end = " ")
-        print()
-        for key, value in kwargs.items():
-            print(f"{key}: {value}")
+fruits = ['apple', 'banana', 'cherry']
 
+for fruit in reversed(fruits):
+    print(fruit)
 
-shipping_label(
-    "1234 Elm St",
-    city="Anytown",
-    state="CA",
-    zip="12345"
-)
