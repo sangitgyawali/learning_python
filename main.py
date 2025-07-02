f06553770f702054df436dd682cfa87af5737096
Sangit Gyawali <developersangeet@gmail.com>
2025-07-02 19:02:05 +0545

    Kwargs in python v2

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # Args and kwargs in Python
 # Kwargs
 
-def print_address(**kwargs):
-    print(type(kwargs))
-    for key, value in kwargs.items():
-        print(f"{key}: {value}")
+def shipping_label(*args , **kwargs):
+    for arg in args:
+        print(arg, end = " ")
+        print()
+        for key, value in kwargs.items():
+            print(f"{key}: {value}")
 
-print_address(
-    street="123 Main St",
+
+shipping_label(
+    "1234 Elm St",
     city="Anytown",
     state="CA",
     zip="12345"
 )
-
