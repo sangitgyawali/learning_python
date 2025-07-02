4a5430028e3c10f20d2555977695cd6eccc9e493
Sangit Gyawali <developersangeet@gmail.com>
2025-07-02 19:17:19 +0545

    Membership operators in Python v4

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 # Membership operators in Python
 
-grades = {
-    "Alice": 85,
-    "Bob": 92,
-    "Charlie": 78
-}
+email = "alice@example.com"
 
-student = input("Enter the student's name: ")
+if "@" in email:
+    print("Email contains '@' symbol.")
 
-if student in grades:
-    print(f"{student} has a grade of {grades[student]}.")
 else:
-    print(f"{student} is not in the gradebook.")
-
+    print("Email does not contain '@' symbol.")
