20bb22f0067ab47727ef6a98e5d08f0176d9561c
Sangit Gyawali <developersangeet@gmail.com>
2025-07-02 18:59:20 +0545

    Kwargs in python

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # Args and kwargs in Python
+# Kwargs
 
-def display_name(*args):
-    for arg in args:
-        print(arg, end=" ")
+def print_address(**kwargs):
+    print(type(kwargs))
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
 
-display_name("Alice", "Bob", "Charlie")
+print_address(
+    street="123 Main St",
+    city="Anytown",
+    state="CA",
+    zip="12345"
+)
+
