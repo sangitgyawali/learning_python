c25c6bf13d814271de9f85cd441e7abac8508390
Sangit Gyawali <developersangeet@gmail.com>
2025-07-03 22:33:36 +0545

    Modules in Python

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-# Match case statement in Python
+# Modules in Python
 
-def day_of_week(day):
-    match day:
-        case "Sunday":
-            return "It's Sunday!"
-        case "Monday":
-            return "It's Monday!"
-        case "Tuesday":
-            return "It's Tuesday!"
-        case "Wednesday":
-            return "It's Wednesday!"
-        case "Thursday":
-            return "It's Thursday!"
-        case "Friday":
-            return "It's Friday!"
-        case "Saturday":
-            return "It's Saturday!"
-        case _:
-            return "Invalid day!"
-        
-print(day_of_week("Monday"))
+pi = 3.14159
+
+def square(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
+
+def circumference(radius):
+    return 2 * pi * radius
+
+print(square(2))
+print(cube(3))
+print(circumference(4))
