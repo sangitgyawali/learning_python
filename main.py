b8103bb9b26fb745b4ed396da88f3b21b41a7527
Sangit Gyawali <developersangeet@gmail.com>
2025-07-05 15:17:09 +0545

    Banking Program in Python

---
+++ main.py
@@ -0,0 +1,38 @@
+# Banking Program in Python
+
+def show_balance():
+    print(f"Your balance is ${balance}")
+
+def deposit():
+    pass
+
+def withdraw():
+    pass
+
+balance = 0
+is_running = True
+
+while is_running:
+    print("Banking Program")
+    print("1.Show Balance")
+    print("2.Deposite")
+    print("3.Withdraw")
+    print("4.Exit")
+
+    choice = input("Enter your choice (1-4):")
+
+    if choice == '1':
+        show_balance
+
+    elif choice == '2':
+        deposit
+
+    elif choice == '3':
+        withdraw
+
+    elif choice == '4':
+        is_running = False
+    else:
+        print("That is not a valid choice")
+
+print("Thank You! Have a nice day")
