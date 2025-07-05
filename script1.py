7fbeace0f4171d190f03b7f931e8201a068bf38d
Sangit Gyawali <developersangeet@gmail.com>
2025-07-05 15:04:07 +0545

    If name statement in python

---
+++ script1.py
@@ -0,0 +1,10 @@
+def favourite_food(food):
+    print(f"Your favorite food is {food}")
+
+def main():
+    print("This is script1")
+    favourite_food("Pizza")
+    print("GoodBye!")
+
+if __name__ == '__main__':
+    main()
