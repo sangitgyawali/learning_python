6609c8aa6b7c0c7f38aa2b2df0c8461d8076f204
Sangit Gyawali <developersangeet@gmail.com>
2025-07-11 15:54:03 +0545

    Encryption Program in Python v2

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ import random
 import string
 
 chars = " " + string.whitespace + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+
+print(f"chars: {chars}")
+print(f"key: {key}")
+
 
 print(chars)
