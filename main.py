52414ebec0150221d7f99f1725395e5fa11aa311
Sangit Gyawali <developersangeet@gmail.com>
2025-07-13 11:42:06 +0545

    Hangman Game in Python v5

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import random
 
 words = ("apple", "orange", "banana", "coconut", "pineapple")
 
-#dictionary of key:()
 hangman_art = {
     0: ("  ", "  ", "  "),
     1: (" o ", "  ", "  "),
@@ -19,24 +18,51 @@ def display_man(wrong_guesses):
         print(line)
 
 def display_hint(hint):
-    pass
+    print("Word: " + " ".join(hint))
 
 def display_answer(answer):
-    pass
+    print("The word was: " + answer)
 
 def main():
     answer = random.choice(words)
     hint = ["_"] * len(answer)
     wrong_guesses = 0
     guessed_letters = set()
+    max_wrong = 6
     is_running = True
-    
-    while(is_running):
+
+    while is_running:
         display_man(wrong_guesses)
         display_hint(hint)
+        print("Guessed letters:", " ".join(sorted(guessed_letters)))
         guess = input("Enter a letter: ").lower()
 
+        if not guess.isalpha() or len(guess) != 1:
+            print("Please enter a single letter.")
+            continue
+
+        if guess in guessed_letters:
+            print("You already guessed that letter.")
+            continue
+
+        guessed_letters.add(guess)
+
+        if guess in answer:
+            for idx, letter in enumerate(answer):
+                if letter == guess:
+                    hint[idx] = guess
+            if "_" not in hint:
+                print("\nCongratulations! You guessed the word!")
+                display_hint(hint)
+                is_running = False
+        else:
+            wrong_guesses += 1
+            print("Wrong guess!")
+            if wrong_guesses == max_wrong:
+                display_man(wrong_guesses)
+                print("\nGame Over!")
+                display_answer(answer)
+                is_running = False
 
 if __name__ == "__main__":
     main()
-
