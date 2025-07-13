b3972886b17c1d7c77763d6a6c73f76257d384b7
Sangit Gyawali <developersangeet@gmail.com>
2025-07-13 13:00:21 +0545

    Super Function in Python

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-# Multiple Inheritence in Python
+# Super Function in Python
 
-class Prey:
-    def flee(self):
-        print("This animal is fleeing")
+class Shape:
+    def __init__(self, color , is_filled):
+        self.color = color
+        self.is_filled = is_filled
+        
+class Circle(Shape):
+    def __init__(self, color, is_filled, radius):
+        super().__init__(color, is_filled)
+        self.radius = radius
+        
+class Square(Shape):
+    def __init__(self, color, is_filled, width):
+        super().__init__(color, is_filled)
+        self.width = width
 
-class Predator:
-    def hunt(self):
-        print("This animal is hunting")
+class Triangle(Shape):
+    def __init__(self, color, is_filled, width, height):
+        super().__init__(color, is_filled)
+        self.width = width
+        self.height = height
 
-class Rabbit(Prey):
-    pass
+circle = Circle(color="red", is_filled=True, radius=5)
+square = Square(color="blue", is_filled=False, width=6)
+triangle = Triangle(color="green", is_filled=True, width=7, height=8)
 
-class Hawk(Predator):
-    pass
-
-class Fish(Prey, Predator):
-    pass
-
-
-rabbit = Rabbit()
-hawk = Hawk()
-fish = Fish()
-
-hawk.hunt()
+print(triangle.color)
+print(triangle.is_filled)
+print(triangle.color)
+print(triangle.is_filled)
+print(f"{triangle.width}cm")
+print(f"{triangle.height}cm")
