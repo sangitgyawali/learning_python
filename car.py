f6357e9de7c2adb71ee9dbefcc556ae9a2cb515e
Sangit Gyawali <developersangeet@gmail.com>
2025-07-13 11:55:33 +0545

    Object in Python

---
+++ car.py
@@ -0,0 +1,6 @@
+class Car:
+    def __init__(self, model, year, color, for_sale):
+        self.model = model 
+        self.year = year
+        self.color = color
+        self.for_sale = for_sale
