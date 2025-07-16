6e4186a00fa3f5d267962ff67dfb7b1e05154c9d
Sangit Gyawali <developersangeet@gmail.com>
2025-07-16 22:30:01 +0545

    Class Methods in Python

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-# Static Methods in Python
+# Class Methods in Python
 
-class Employee:
+class Student:
 
-    def __init__(self, name, position):
+    count = 0
+    total_gpa = 0
+
+    def __init__(self, name, gpa):
         self.name = name
-        self.position = position
+        self.gpa = gpa
+        Student.count += 1
+        Student.total_gpa += gpa
 
+    # INSTANCE METHOD
     def get_info(self):
-        return f"{self.name} = {self.position}"
-    
-    @staticmethod
-    def is_valid_position(position):
-        valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
-        return position in valid_positions
-    
-employee1 = Employee("Eugune", "Manager")
-employee2 = Employee("John", "Cashier")
-employee3 = Employee("Mary", "Cook")
+        return f"{self.name} {self.gpa}"
 
-print(Employee.is_valid_position("Rocket Scientist"))
-print(employee1.get_info())
-print(employee2.get_info())
-print(employee3.get_info())
+    @classmethod
+    def get_count(cls):
+        return f"Total # of students: {cls.count}"
+
+    @classmethod
+    def get_average_gpa(cls):
+        if cls.count == 0:
+            return 0
+        else:
+            return f"{cls.total_gpa / cls.count}"
+        
+student1 = Student("Spongebob",3.2)
+student2 = Student("Patrick",3.0)
+student3 = Student("Sandy",4.0)
+
+print(Student.get_count())
+print(Student.get_average_gpa())
