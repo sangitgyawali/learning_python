9421c31b14667709d43a7a31e30b03000a528e42
Sangit Gyawali <developersangeet@gmail.com>
2025-07-18 22:21:48 +0545

    Magic Methods in Python v3

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ class Book:
     def __eq__(self, other):
         return self.title == other.title and self.author == other.author
 
+    def __lt__(self, other):
+        return self.num_pages < other.num_pages
+
 book1 = Book("The Hobbit", "J.R.R. Tolkien", 310)
 book2 = Book("1984", "George Orwell", 328)
 book3 = Book("To Kill a Mockingbird", "Harper Lee", 281)
 
-
-print(book3)
-print(book1 == book2)
-
+print(book2 < book3)
