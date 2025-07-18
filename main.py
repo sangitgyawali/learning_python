d50a5200443b1771c76a88282852d0f97b554468
Sangit Gyawali <developersangeet@gmail.com>
2025-07-18 22:37:31 +0545

    Property Decorator in Python v3

--- main.py
+++ main.py
@@ -12,8 +12,18 @@ class Rectangle:
     @property
     def height(self):
         return f"{self._height:.1f}cm"
+    
+    @width.setter
+    def width(self, new_width):
+        if new_width > 0:
+            self._width = new_width
+
+        else:
+            print("height must be greater than zero")
 
 rectangle = Rectangle(3, 4)
+
+rectangle.width = 0
 
 print(rectangle._width)
 print(rectangle.height)
