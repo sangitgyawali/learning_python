2ac418ebf0b99efaffe4c050f36da7ce18654f4f
Sangit Gyawali <developersangeet@gmail.com>
2025-07-20 19:54:24 +0545

    File Detection in Python v2

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ file_path = "test.txt"
 if os.path.exists(file_path):
     print(f"The location '{file_path}' exists")
 
+    if os.path.isfile(file_path):
+        print("That is a file")
+    elif ps.path.isdir(file_path):
+        print("This is a directory")
 else:
     print("That location doesn't exist")
