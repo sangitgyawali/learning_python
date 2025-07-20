050d481a2a2e5cac36eb749fa182648e4b0b3e47
Sangit Gyawali <developersangeet@gmail.com>
2025-07-20 19:13:39 +0545

    Decorator in Python v2

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-# Decorator
-
+# Decorator in Python
 
 def add_sprinkles(func):
     def wrapper():
         print("You add sprinkles")
         func()
     return wrapper
+
+def add_fudge(func):
+    def wrapper():
+        print("You add fudge")
+        func()
+    return wrapper
         
 @add_sprinkles
+@add_fudge
 def get_ice_cream():
     print("Here is your ice cream")
 
