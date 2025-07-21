8a20a14c01966fc4ffe0e0e9c51b5ad15660a6fe
Sangit Gyawali <developersangeet@gmail.com>
2025-07-21 22:22:29 +0545

    File Writing in Python v4

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ file_path = "output.txt"
 try:
     with open(file_path, "w") as file:
         for employee in employees:
-
+            file.write(employee)
+        print(f"txt file '{file_path}' was created")
 
 except FileExistsError:
     print("That file already exists!")
