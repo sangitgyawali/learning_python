8393059ba359a71e543f4fd427adb9859a99d918
Sangit Gyawali <developersangeet@gmail.com>
2025-07-21 22:20:19 +0545

    File Writing in Python v2

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # File Writing in Python
 
-txt_data = "I like pizza!"
+employees = ["Eugene", "Squidward", "Spongebob", "Patrick"]
 
 file_path = "output.txt"
 
 with open(file_path, "w") as file:
     file.write(txt_data)
     print(f"txt file '{file_path}' was created")
+except FileExistsError:
+    print("That file already exists!")
