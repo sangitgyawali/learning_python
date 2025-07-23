7cc6f58202593ca7dcaaab01d723c98b2a02d55b
Sangit Gyawali <developersangeet@gmail.com>
2025-07-23 22:48:58 +0545

    Request API Python v2

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ base_url = "https://pokeapi.co/api/v2/"
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{name}"
     response = requests.get(url)
-    print(response)
+    pokemon_data = response.json()
+    print(pokemon_data)
+    return pokemon_data
+
+    if response.status.code == 200:
+        print("Data retrieved!")
+    else:
+        print(f"'Failed to retrive data {response.status_code}")
 
 pokemon_name = "pikachu"
-get_pokemon_info(pokemon_name)
+pokemon_info = get_pokemon_info(pokemon_name)
